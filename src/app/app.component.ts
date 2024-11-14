a5927f5fad82e9d60806df9bbcb7fb69cf4ab821
Krishna Murali Andiboyina <murali.andiboyina@evolutyz.com>
2024-11-14 17:11:23 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import csc from 'countries-states-cities';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'country-state-city';
+  countries: any[] = [];
+  states: any[] = [];
+  cities: any[] = [];
+  selectedCountryId: string | null = null;
+  selectedStateId: string | null = null;
+  selectedCityId: string | null = null;
+
+  ngOnInit(): void {
+    this.countries = csc.getAllCountries();
+  }
+
+  getStates(event: Event): void {
+    const countryId = (event.target as HTMLSelectElement).value;
+    this.selectedStateId = null;
+    this.selectedCityId = null;
+    this.selectedCountryId = countryId;
+    this.states = csc.getStatesOfCountry(Number(countryId));
+    this.cities = [];
+  }
+
+  getCities(event: Event): void {
+    const stateId = (event.target as HTMLSelectElement).value;
+    this.selectedCityId = null;
+    this.selectedStateId = stateId;
+    this.cities = csc.getCitiesOfState(Number(stateId));
+  }
 }
